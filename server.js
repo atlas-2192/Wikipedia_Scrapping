f5a4345f6e3ad9d48a7602c8ee8a496d832016a2
atlas-2192 <sunstar962090@gmail.com>
2015-10-21 13:36:30 -0400

    template'

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var express = require('express'),
     publicDir = process.argv[2] || __dirname + '/public',
     favicon = require('serve-favicon'),
     fs = require('fs');
+
+var template = _.template(fs.readFileSync("public/template.html"))
+
 // config = require('./config');
 function genContent(seed, cb) {
     var child = child_process.exec('cd .. ; th sample.lua cv/currentmodel.t7 -gpuid -1 -temperature .8 -length 3000 -primetext "' + seed + '" > wikiScrape/page/' + seed);
@@ -27,60 +30,67 @@ function genContent(seed, cb) {
 }
 // genContent(console.log)
 // http: //en.wikipedia.org/wiki
-    var createServer = function(port) {
+var createServer = function(port) {
 
-        app.use(favicon(__dirname + '/favicon.ico'));
-        app.get('/', function(req, res) {
-            fs.readFile("public/index.html", function(err, data) {
-                if (err) {
-                    throw err;
-                }
-                res.set('Content-Type', 'text/html');
-                res.send(data);
-            });
-        })
-        app.get('/page/*', function(req, res) {
-            console.log(req.url)
-            var filename = req.url.slice(1);
+    app.use(favicon(__dirname + '/favicon.ico'));
+    app.get('/', function(req, res) {
+        fs.readFile("public/index.html", function(err, data) {
+            if (err) {
+                throw err;
+            }
             res.set('Content-Type', 'text/html');
-
-            fs.readFile(filename, function(err, data) {
-                if (err) {
-                    genContent(req.url.replace("/page/", ""), () => {
-                        console.log("called")
-                        fs.readFile(filename, function(err, data) {
-                            if (err) {
-                                throw err;
-                            }
-                            var regex = new RegExp('http[s]?://en.wikipedia.org/wiki/', "g");
-                            data = data.toString().replace(regex, "")
-                           // regex = new RegExp('http://en.wikipedia.org/wiki', "g");
-                           // data = data.toString().replace(regex, "page/")
-
-                            res.send(data);
-                        });
-                    })
-
-                } else //file exists, send it
-                    res.send(data);
-
-            });
+            res.send(data);
+        });
+    })
 
 
+    app.get('/page/*', function(req, res) {
+        console.log(req.url)
+        var filename = req.url.slice(1);
+        res.set('Content-Type', 'text/html');
 
-        })
+        fs.readFile(filename, function(err, data) {
+            if (err) {
+                genContent(req.url.replace("/page/", ""), () => {
+                    console.log("called")
+                    fs.readFile(filename, function(err, data) {
+                        if (err) {
+                            throw err;
+                        }
+                        var regex = new RegExp('http[s]?://en.wikipedia.org/wiki/', "g");
+                        data = data.toString().replace(regex, "")
 
-        // app.use('/static/', express.static(path.join(__dirname, 'public')));
 
-        var server = app.listen(port, function() {
+                        // regex = new RegExp('http://en.wikipedia.org/wiki', "g");
+                        // data = data.toString().replace(regex, "page/")
 
-            var host = server.address().address;
-            var port = server.address().port;
+                        res.send(tpl({
+                            page: req.url.replace("/page/", ""),
+                            conent: data
+                        }));
+                    });
+                })
 
-            console.log('Voynich listening at http://%s:%s', host, port);
+            } else //file exists, send it
+                res.send(data);
 
         });
-    };
+
+
+
+    })
+
+    // app.use('/static/', express.static(path.join(__dirname, 'public')));
+
+    var server = app.listen(port, function() {
+
+        var host = server.address().address;
+        var port = server.address().port;
+
+        console.log('Voynich listening at http://%s:%s', host, port);
+
+    });
+};
 
 module.exports = createServer;
 
