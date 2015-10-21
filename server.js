78a85857acfaf515a20007a43120a73601edc429
atlas-2192 <sunstar962090@gmail.com>
2015-10-20 22:46:04 -0400

    ahhh

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express'),
     request = require('request'),
     child_process = require('child_process'),
     hostname = process.env.HOSTNAME || 'localhost',
+    cheerio = require('cheerio'),
+
     // port = process.env.PORT || 4567,
     publicDir = process.argv[2] || __dirname + '/public',
     favicon = require('serve-favicon'),
@@ -24,49 +26,52 @@ function genContent(seed, cb) {
 
 }
 // genContent(console.log)
+http: //en.wikipedia.org/wiki
+    var createServer = function(port) {
 
-var createServer = function(port) {
+        app.use(favicon(__dirname + '/db/favicon.ico'));
 
-    app.use(favicon(__dirname + '/db/favicon.ico'));
+        app.get('/*', function(req, res) {
+            console.log(req.url)
+            var filename = 'db/' + req.url.slice(1);
+            res.set('Content-Type', 'text/html');
 
-    app.get('/*', function(req, res) {
-        console.log(req.url)
-        var filename = 'db/' + req.url.slice(1);
-        res.set('Content-Type', 'text/html');
+            fs.readFile(filename, function(err, data) {
+                if (err) {
+                    genContent(req.url.slice(1), () => {
+                        console.log("called")
+                        fs.readFile(filename, function(err, data) {
+                            if (err) {
+                                throw err;
+                            }
 
-        fs.readFile(filename, function(err, data) {
-            if (err) {
-                genContent(req.url.slice(1), () => {
-                    console.log("called")
-                    fs.readFile(filename, function(err, data) {
-                        if (err) {
-                            throw err;
-                        }
-                        res.send(data);
-                    });
-                })
+                            ;
 
-            } else //file exists, send it
-                res.send(data);
+                            res.send(html.replace("http://en.wikipedia.org/wiki", ""));
+                        });
+                    })
+
+                } else //file exists, send it
+                    res.send(data);
+
+            });
+
+
+
+        })
+
+        // app.use('/static/', express.static(path.join(__dirname, 'public')));
+
+        var server = app.listen(port, function() {
+
+            var host = server.address().address;
+            var port = server.address().port;
+
+            console.log('Voynich listening at http://%s:%s', host, port);
 
         });
-
-
-
-    })
-
-    // app.use('/static/', express.static(path.join(__dirname, 'public')));
-
-    var server = app.listen(port, function() {
-
-        var host = server.address().address;
-        var port = server.address().port;
-
-        console.log('Voynich listening at http://%s:%s', host, port);
-
-    });
-};
+    };
 
 module.exports = createServer;
 
-createServer(8080)
+createServer(80)
